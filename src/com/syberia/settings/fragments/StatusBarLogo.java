Project-Xtended/packages_apps_Xtensions

[SQUASHED] Statusbar logo and positions [2/2]

src/com/msm/xtended/fragments/StatusBarLogo.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2018 MSM-Xtended Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.msm.xtended.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class StatusBarLogo extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private ListPreference mShowLogo;
+    private ListPreference mLogoStyle;
+    private ColorPickerPreference mStatusBarLogoColor;
+    static final int DEFAULT_LOGO_COLOR = 0xff009688;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.x_statusbar_logo);
+
+        mShowLogo = (ListPreference) findPreference("status_bar_logo");
+        mShowLogo.setOnPreferenceChangeListener(this);
+        int showLogo = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO,
+                0, UserHandle.USER_CURRENT);
+        mShowLogo.setValue(String.valueOf(showLogo));
+        mShowLogo.setSummary(mShowLogo.getEntry());
+
+        mLogoStyle = (ListPreference) findPreference("status_bar_logo_style");
+        mLogoStyle.setOnPreferenceChangeListener(this);
+        int logoStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE,
+                0, UserHandle.USER_CURRENT);
+        mLogoStyle.setValue(String.valueOf(logoStyle));
+        mLogoStyle.setSummary(mLogoStyle.getEntry());
+
+        mStatusBarLogoColor = (ColorPickerPreference) findPreference("status_bar_logo_color");
+        mStatusBarLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_COLOR, DEFAULT_LOGO_COLOR);
+        String hexColor = String.format("#%08x", (DEFAULT_LOGO_COLOR & intColor));
+        mStatusBarLogoColor.setSummary(hexColor);
+        mStatusBarLogoColor.setNewPreviewColor(intColor);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mShowLogo)) {
+            int showLogo = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO, showLogo, UserHandle.USER_CURRENT);
+            int index = mShowLogo.findIndexOfValue((String) newValue);
+            mShowLogo.setSummary(
+                    mShowLogo.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mLogoStyle)) {
+            int logoStyle = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_STYLE, logoStyle, UserHandle.USER_CURRENT);
+            int index = mLogoStyle.findIndexOfValue((String) newValue);
+            mLogoStyle.setSummary(
+                    mLogoStyle.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mStatusBarLogoColor)) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_LOGO_COLOR, intHex);
+            return true;
+        }
+        return false;
+    }
+}
+

Danish1999/platform_packages_apps_SyberiaSettings

src/com/syberia/settings/fragments/StatusBarLogo.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2018 MSM-Xtended Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.Utils;
+
+public class StatusBarLogo extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private ListPreference mShowLogo;
+    private ListPreference mLogoStyle;
+    private ColorPickerPreference mStatusBarLogoColor;
+    static final int DEFAULT_LOGO_COLOR = 0xffff8800;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar_logo);
+
+        mShowLogo = (ListPreference) findPreference("status_bar_logo");
+        mShowLogo.setOnPreferenceChangeListener(this);
+        int showLogo = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO,
+                0, UserHandle.USER_CURRENT);
+        mShowLogo.setValue(String.valueOf(showLogo));
+        mShowLogo.setSummary(mShowLogo.getEntry());
+
+        mLogoStyle = (ListPreference) findPreference("status_bar_logo_style");
+        mLogoStyle.setOnPreferenceChangeListener(this);
+        int logoStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE,
+                0, UserHandle.USER_CURRENT);
+        mLogoStyle.setValue(String.valueOf(logoStyle));
+        mLogoStyle.setSummary(mLogoStyle.getEntry());
+
+        mStatusBarLogoColor = (ColorPickerPreference) findPreference("status_bar_logo_color");
+        mStatusBarLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_COLOR, DEFAULT_LOGO_COLOR);
+        String hexColor = String.format("#%08x", (DEFAULT_LOGO_COLOR & intColor));
+        mStatusBarLogoColor.setSummary(hexColor);
+        mStatusBarLogoColor.setNewPreviewColor(intColor);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mShowLogo)) {
+            int showLogo = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO, showLogo, UserHandle.USER_CURRENT);
+            int index = mShowLogo.findIndexOfValue((String) newValue);
+            mShowLogo.setSummary(
+                    mShowLogo.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mLogoStyle)) {
+            int logoStyle = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_LOGO_STYLE, logoStyle, UserHandle.USER_CURRENT);
+            int index = mLogoStyle.findIndexOfValue((String) newValue);
+            mLogoStyle.setSummary(
+                    mLogoStyle.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mStatusBarLogoColor)) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_LOGO_COLOR, intHex);
+            return true;
+        }
+        return false;
+    }
+}
+