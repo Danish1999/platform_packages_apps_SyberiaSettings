Project-Xtended/packages_apps_Xtensions

Lockscreen Visualizer: Add pulse magic [2/2]
**SDB- Also Make it separate preference

src/com/msm/xtended/fragments/VisualizerUI.java
@@ -1,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.msm.xtended.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class VisualizerUI extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KEY_AUTOCOLOR = "lockscreen_visualizer_autocolor";
+    private static final String KEY_LAVALAMP = "lockscreen_lavalamp_enabled";
+
+    private SwitchPreference mAutoColor;
+    private SwitchPreference mLavaLamp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.x_visualizer_ui);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean mLavaLampEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+
+        mAutoColor = (SwitchPreference) findPreference(KEY_AUTOCOLOR);
+        mAutoColor.setEnabled(!mLavaLampEnabled);
+
+        if (mLavaLampEnabled) {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_lavalamp));
+        } else {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_summary));
+        }
+
+        mLavaLamp = (SwitchPreference) findPreference(KEY_LAVALAMP);
+        mLavaLamp.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLavaLamp) {
+            boolean mLavaLampEnabled = (Boolean) newValue;
+            if (mLavaLampEnabled) {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_lavalamp));
+            } else {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_summary));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.XTENSIONS;
+    }
+}
+

Danish1999/platform_packages_apps_SyberiaSettings

src/com/syberia/settings/fragments/VisualizerUI.java
@@ -1,0 +1,90 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class VisualizerUI extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KEY_AUTOCOLOR = "lockscreen_visualizer_autocolor";
+    private static final String KEY_LAVALAMP = "lockscreen_lavalamp_enabled";
+
+    private SwitchPreference mAutoColor;
+    private SwitchPreference mLavaLamp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.x_visualizer_ui);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean mLavaLampEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+
+        mAutoColor = (SwitchPreference) findPreference(KEY_AUTOCOLOR);
+        mAutoColor.setEnabled(!mLavaLampEnabled);
+
+        if (mLavaLampEnabled) {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_lavalamp));
+        } else {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_summary));
+        }
+
+        mLavaLamp = (SwitchPreference) findPreference(KEY_LAVALAMP);
+        mLavaLamp.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLavaLamp) {
+            boolean mLavaLampEnabled = (Boolean) newValue;
+            if (mLavaLampEnabled) {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_lavalamp));
+            } else {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_summary));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SYBERIA;
+    }
+}
+